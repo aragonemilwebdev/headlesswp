f46da3cd05b5de84f24b70c16aa4b2e90ae81a9e
aragonemilwebdev <aragonemil.webdev@gmail.com>
2022-12-10 12:03:49 +0800

    bg image not showing

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -4,11 +4,14 @@ export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
       case "core/cover": {
-        return 
-          <Cover key={block.id} background={block.attributes.url}>
-            core cover
-          </Cover>
-       
+        return (
+          <>
+                      <Cover key={block.id} background={block.attributes.url}>
+          core cover
+        </Cover>
+          </>
+
+        )
       }
       default:
         return null;
--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -1,3 +1,14 @@
 export const Cover = ({ children, background }) => {
-  return <div className=" h-screen bg-red-500">{children}</div>
+  return (
+    <div className=" h-screen bg-slate-800 relative min-h-[400px] flex justify-center items-start">
+      <Image
+        alt="cover"
+        src={background}
+        layout="fill"
+        objectFit="cover"
+        className=" mix-blend-soft-light"
+      />
+      {children}
+    </div>
+  );
 };
